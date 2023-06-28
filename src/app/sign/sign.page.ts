2c447d4ee937419daf8879e6b2557503b0db1879
lhudymuel <lhudymuel@gmail.com>
2023-06-28 17:11:18 +0800

    Added my project

---
+++ src/app/sign/sign.page.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-sign',
+  templateUrl: './sign.page.html',
+  styleUrls: ['../login/login.page.scss'],
+})
+export class SignPage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
